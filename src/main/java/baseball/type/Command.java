c3197ded545e8d39721db2c8dfb63b0a791ea6df
desktop <jeongwon.park.0314@gmail.com>
2021-10-04 03:06:32 +0900

    fix: change command

--- src/main/java/baseball/type/Command.java
+++ src/main/java/baseball/type/Command.java
@@ -1,5 +1,9 @@
 package baseball.type;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public enum Command {
     PLAY("1"),
     EXIT("2");
@@ -8,6 +12,18 @@ public enum Command {
 
     Command(String value) {
         this.value = value;
+    }
+
+    public static Optional<Command> commandOf(String value) {
+        return Optional.ofNullable(getCommandMap().get(value));
+    }
+
+    private static Map<String, Command> getCommandMap() {
+        Map<String, Command> commandMap = new HashMap<>();
+        for (int i = 0; i < Command.values().length; i++) {
+            commandMap.put(Command.values()[i].value, Command.values()[i]);
+        }
+        return commandMap;
     }
 
     public String getValue() {
