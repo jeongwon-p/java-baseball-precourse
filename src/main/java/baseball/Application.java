306b0a850aca41fac5071a70980503185d45e28d
desktop <jeongwon.park.0314@gmail.com>
2021-10-04 14:07:06 +0900

    feat: play the game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
 public class Application {
+
+    private static final int NUMBER_LENGTH = 3;
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        Player player = new Player();
+
+        do {
+            Opponent opponent = new Opponent(NUMBER_LENGTH);
+            Game game = new Game(opponent, player, NUMBER_LENGTH);
+            game.play();
+        } while (player.playNewGame());
     }
 }
